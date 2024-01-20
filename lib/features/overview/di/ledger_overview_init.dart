7f450b486542ad78abfd1af94041b8a0326af55d
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 19:18:24 +0600

    Update the registration of LedgerOverviewUseCase and LedgerOverviewBloc in dependency injection container.

--- lib/features/overview/di/ledger_overview_init.dart
+++ lib/features/overview/di/ledger_overview_init.dart
@@ -2,9 +2,11 @@ import '../../../core/di/dependencies_setup.dart';
 import '../data/repo/ledger_overview_repo_impl.dart';
 import '../domain/repo/ledger_overview_repo.dart';
 import '../domain/use_case/ledger_overview_use_case.dart';
+import '../presentation/bloc/ledger_overview_bloc.dart';
 
 void ledgerOverviewInit() {
   getIt
     ..registerSingleton<LedgerOverviewRepo>(LedgerOverviewRepoImpl(getIt(), getIt()))
-    ..registerSingleton<LedgerOverviewUseCase>(LedgerOverviewUseCase(getIt()));
+    ..registerSingleton<LedgerOverviewUseCase>(LedgerOverviewUseCase(getIt()))
+    ..registerFactory<LedgerOverviewBloc>(() => LedgerOverviewBloc(getIt()));
 }
