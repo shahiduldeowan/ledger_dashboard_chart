2643810140c0c6cb0bb74e419ff055431402d79f
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 17:17:38 +0600

    Update dependency injection in ledger feature

--- lib/core/di/dependencies_setup.dart
+++ lib/core/di/dependencies_setup.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../features/ledger/data/source/ledger_api_service.dart';
 import '../handler/api_handler.dart';
 import '../handler/api_handler_impl.dart';
 import '../interceptor/dio_interceptor.dart';
@@ -10,5 +11,6 @@ final GetIt getIt = GetIt.instance;
 Future<void> dependenciesInjection() async {
   getIt
     ..registerLazySingleton<Dio>(() => Dio()..interceptors.add(DioInterceptor()))
-    ..registerLazySingleton<ApiHandler>(() => ApiHandlerImpl());
+    ..registerLazySingleton<ApiHandler>(() => ApiHandlerImpl())
+    ..registerSingleton<LedgerApiService>(LedgerApiService(getIt()));
 }
