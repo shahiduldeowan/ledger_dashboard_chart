64e52e82b9f0af2085383a0cb84daba27dbd51fa
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 18:50:38 +0600

    Update imports and initialize ledger current month and overview

--- lib/core/di/dependencies_setup.dart
+++ lib/core/di/dependencies_setup.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../features/current_month/di/ledger_current_month_init.dart';
 import '../../features/ledger/data/source/ledger_api_service.dart';
+import '../../features/overview/di/ledger_overview_init.dart';
 import '../handler/api_handler.dart';
 import '../handler/api_handler_impl.dart';
 import '../interceptor/dio_interceptor.dart';
@@ -13,4 +15,7 @@ Future<void> dependenciesInjection() async {
     ..registerLazySingleton<Dio>(() => Dio()..interceptors.add(DioInterceptor()))
     ..registerLazySingleton<ApiHandler>(() => ApiHandlerImpl())
     ..registerSingleton<LedgerApiService>(LedgerApiService(getIt()));
+
+  ledgerCurrentMonthInit();
+  ledgerOverviewInit();
 }
