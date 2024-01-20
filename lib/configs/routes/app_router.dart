123b0aae5820a3339915af296d8c70caeee6d94a
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 13:47:27 +0600

    Update AppRouter to include LedgerPage route

--- lib/configs/routes/app_router.dart
+++ lib/configs/routes/app_router.dart
@@ -1,1 +1,12 @@
-class AppRouter {}
+import 'package:flutter/material.dart';
+
+import '../../features/ledger/presentation/page/ledger_page.dart';
+
+class AppRouter {
+  static Route? onGenerateRoute(RouteSettings settings) => switch (settings.name) {
+        LedgerPage.routeName => _route(const LedgerPage()),
+        _ => null,
+      };
+
+  static Route _route(Widget page) => MaterialPageRoute(builder: (_) => page);
+}
