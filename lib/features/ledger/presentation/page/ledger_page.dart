6dd322a996dfb44d777c86e3b69e6f48e303c35c
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 14:21:47 +0600

    Update ledger dashboard scaffold and import statement.

--- lib/features/ledger/presentation/page/ledger_page.dart
+++ lib/features/ledger/presentation/page/ledger_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+import '../../../../core/constants/strings.dart';
+
 class LedgerPage extends HookWidget {
   const LedgerPage({super.key});
 
@@ -9,7 +11,10 @@ class LedgerPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(Strings.ledgerDashboardTitle),
+      ),
       body: Center(
         child: Icon(CupertinoIcons.arrow_3_trianglepath),
       ),
