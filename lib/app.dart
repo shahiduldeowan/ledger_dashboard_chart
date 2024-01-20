767c2c869b3c493075ff051ccfa47d0fc565d6cb
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 19:21:26 +0600

    Update widget build method and imports in Dart file

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'configs/routes/app_router.dart';
 import 'configs/themes/app_dark_theme.dart';
+import 'core/di/dependencies_setup.dart';
 import 'features/ledger/presentation/page/ledger_page.dart';
+import 'features/overview/presentation/bloc/ledger_overview_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Ledger Dashboard Chart',
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.dark,
-        darkTheme: appDarkTheme,
-        initialRoute: LedgerPage.routeName,
-        onGenerateRoute: AppRouter.onGenerateRoute,
+  Widget build(BuildContext context) => MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => getIt<LedgerOverviewBloc>()..add(const GetLedgerOverviewEvent())),
+        ],
+        child: MaterialApp(
+          title: 'Ledger Dashboard Chart',
+          debugShowCheckedModeBanner: false,
+          themeMode: ThemeMode.dark,
+          darkTheme: appDarkTheme,
+          initialRoute: LedgerPage.routeName,
+          onGenerateRoute: AppRouter.onGenerateRoute,
+        ),
       );
 }
