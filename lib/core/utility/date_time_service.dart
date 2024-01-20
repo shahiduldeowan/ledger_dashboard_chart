50d6b1a876b5b2d675219eb8c9cd8203601f0783
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 17:48:11 +0600

    Add DateTimeService

---
+++ lib/core/utility/date_time_service.dart
@@ -0,0 +1,5 @@
+extension DateTimeService on DateTime {
+  DateTime get fastDayOfTheMonth => DateTime(year, month, 0, 24, 01, 00, 000);
+
+  DateTime get lastDayOfTheMonth => DateTime(year, month + 1, 0, 23, 59, 59, 999);
+}
