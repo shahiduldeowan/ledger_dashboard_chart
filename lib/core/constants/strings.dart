6acaaa634400139f68be12fdc5447dbb9066ee11
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 19:22:37 +0600

    Add appTitle constant for Ledger Dashboard Chart

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,4 +1,5 @@
 class Strings {
+  static const String appTitle = 'Ledger Dashboard Chart';
   static const String ledgerDashboardTitle = 'Ledger Dashboard';
   static const int creditKey = 113;
   static const String ledgerPredefinedDateString = '1900-01-01';
