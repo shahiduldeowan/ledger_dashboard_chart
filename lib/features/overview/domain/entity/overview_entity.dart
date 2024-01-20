fe1367b103cd5c0e13abf7f8962f23a8b3925657
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 15:21:20 +0600

    Refactor OverviewEntity class to extend LedgerEntity and include LedgerBalanceService.

--- lib/features/overview/domain/entity/overview_entity.dart
+++ lib/features/overview/domain/entity/overview_entity.dart
@@ -1,3 +1,11 @@
 import '../../../ledger/domain/entity/ledger_entity.dart';
+import '../../../ledger/domain/ledger_balance_service.dart';
 
-class OverviewEntity extends LedgerEntity {}
+class OverviewEntity extends LedgerEntity with LedgerBalanceService {
+  const OverviewEntity({
+    super.bill,
+    super.balance,
+    super.payableAmount,
+    super.creditLimit,
+  });
+}
