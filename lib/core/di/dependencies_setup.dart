66f3065bcfdb0f29eda40ceedca96d11565f6ded
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 16:11:28 +0600

    Update dependencies injection for API handling

--- lib/core/di/dependencies_setup.dart
+++ lib/core/di/dependencies_setup.dart
@@ -1,5 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+
+import '../handler/api_handler.dart';
+import '../handler/api_handler_impl.dart';
 
 final GetIt getIt = GetIt.instance;
 
-Future<void> dependenciesInjection() async {}
+Future<void> dependenciesInjection() async {
+  getIt
+    ..registerLazySingleton<Dio>(() => Dio())
+    ..registerLazySingleton<ApiHandler>(() => ApiHandlerImpl());
+}
