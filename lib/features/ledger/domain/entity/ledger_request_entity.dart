53f6282f7d385128893da5c4777513b2897c53bf
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 17:09:09 +0600

    add LedgerRequestEntity

---
+++ lib/features/ledger/domain/entity/ledger_request_entity.dart
@@ -0,0 +1,9 @@
+import 'ledger_entity.dart';
+
+class LedgerRequestEntity extends LedgerEntity {
+  const LedgerRequestEntity({
+    super.fromDate,
+    super.toDate,
+    super.departmentKey,
+  });
+}
