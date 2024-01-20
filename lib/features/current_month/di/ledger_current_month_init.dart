46b3ca2dc209b0ecf3a262b4a4ac8509b1b386df
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 18:48:54 +0600

    add ledgerCurrentMonthInit method

---
+++ lib/features/current_month/di/ledger_current_month_init.dart
@@ -0,0 +1,10 @@
+import '../../../core/di/dependencies_setup.dart';
+import '../data/repo/ledger_current_month_repo_impl.dart';
+import '../domain/repo/ledger_current_month_repo.dart';
+import '../domain/use_case/ledger_current_month_use_case.dart';
+
+void ledgerCurrentMonthInit() {
+  getIt
+    ..registerSingleton<LedgerCurrentMonthRepo>(LedgerCurrentMonthRepoImpl(getIt(), getIt()))
+    ..registerSingleton<LedgerCurrentMonthUseCase>(LedgerCurrentMonthUseCase(getIt()));
+}
