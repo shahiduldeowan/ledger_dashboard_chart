83c38c7af9c3beadf4d360511592a651a92e5bd8
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-20 16:03:12 +0600

    Refactor makeApiCall method to incorporate DataState and ApiCallBack

--- lib/core/handler/api_handler.dart
+++ lib/core/handler/api_handler.dart
@@ -1,7 +1,9 @@
 import 'package:retrofit/retrofit.dart';
 
+import '../resources/data_state.dart';
+
 typedef ApiCallBack<T> = Future<HttpResponse<T>> Function();
 
 abstract class ApiHandler {
-  Future makeApiCall();
+  Future<DataState<T>> makeApiCall<T>(ApiCallBack<T> callBack);
 }
