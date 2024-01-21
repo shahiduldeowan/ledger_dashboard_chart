4cbdb60b5e2443e6848aacdb5c0fed0155c2e2ac
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 16:19:00 +0600

    add constants for total due, credit limit, and current uses.

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -4,4 +4,7 @@ class Strings {
   static const int creditKey = 113;
   static const String ledgerPredefinedDateString = '1900-01-01';
   static const String remain = 'Remain';
+  static const String totalDue = 'Total Due';
+  static const String creditLimit = 'Credit Limit';
+  static const String currentUses = 'Current Uses';
 }
