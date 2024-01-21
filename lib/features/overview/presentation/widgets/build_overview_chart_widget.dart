f3ad77097d6ac5677b6448a386f2cf844e9f788d
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 13:53:32 +0600

    add BuildOverviewChart widget

---
+++ lib/features/overview/presentation/widgets/build_overview_chart_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/entity/ledger_overview_entity.dart';
+
+class BuildOverviewChart extends StatelessWidget {
+  const BuildOverviewChart({super.key, required this.overview});
+
+  final LedgerOverviewEntity overview;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
