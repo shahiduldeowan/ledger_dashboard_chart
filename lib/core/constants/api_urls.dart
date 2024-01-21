5800e7cb1ac095b17cad3510259d4269901550df
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 19:19:29 +0600

    add api_urls.dart file

---
+++ lib/core/constants/api_urls.dart
@@ -0,0 +1,7 @@
+const String baseUrl = 'http://192.168.1.112:9009'; // production
+
+const String path = '/de-clubapi';
+
+const String previousLedgerEndpoint = '$path/api/v1/ledger/previous';
+const String ledgerEndpoint = '$path/api/v1/ledger';
+const String ledgerDetailsEndpoint = '$path/api/v1/ledger/ledger_details';
