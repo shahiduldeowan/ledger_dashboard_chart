6dcbca1452843293149df2279380f1ef11f3dd47
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 13:18:09 +0600

    Add import statement and BlocProvider for LedgerCurrentMonthBloc

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'configs/routes/app_router.dart';
 import 'configs/themes/app_dark_theme.dart';
 import 'core/constants/strings.dart';
 import 'core/di/dependencies_setup.dart';
+import 'features/current_month/presentation/bloc/ledger_current_month_bloc.dart';
 import 'features/ledger/presentation/page/ledger_page.dart';
 import 'features/overview/presentation/bloc/ledger_overview_bloc.dart';
 
@@ -15,6 +16,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) => MultiBlocProvider(
         providers: [
           BlocProvider(create: (context) => getIt<LedgerOverviewBloc>()..add(const GetLedgerOverviewEvent())),
+          BlocProvider(create: (context) => getIt<LedgerCurrentMonthBloc>()..add(const GetLedgerCurrentMonthEvent())),
         ],
         child: MaterialApp(
           title: Strings.appTitle,
