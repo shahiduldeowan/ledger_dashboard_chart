f81e433086307af77a5a68600533e33507a9bb53
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 14:37:29 +0600

    Update the implementation of a widget to show a CircularProgressIndicator

--- lib/features/overview/presentation/widgets/build_overview_chart_widget.dart
+++ lib/features/overview/presentation/widgets/build_overview_chart_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/constants/app_sizes.dart';
 import '../../domain/entity/ledger_overview_entity.dart';
 
 class BuildOverviewChart extends StatelessWidget {
@@ -9,6 +10,36 @@ class BuildOverviewChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(AppSizes.cardContentPadding),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Spacer(
+                flex: 2,
+              ),
+              Expanded(
+                child: Center(
+                  child: FractionallySizedBox(
+                    heightFactor: 0.65,
+                    widthFactor: 1,
+                    child: CircularProgressIndicator(
+                      value: overview.getRemainWithPercent,
+                      strokeCap: StrokeCap.round,
+                      strokeWidth: 8,
+                      backgroundColor: Colors.white12,
+                      color: null,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
