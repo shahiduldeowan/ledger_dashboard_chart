bd9dc5330e1294abe02249b95051b6aa59f0f706
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 13:15:45 +0600

    Update dependencies and registration for LedgerCurrentMonth in Bloc.

--- lib/features/current_month/di/ledger_current_month_init.dart
+++ lib/features/current_month/di/ledger_current_month_init.dart
@@ -2,9 +2,11 @@ import '../../../core/di/dependencies_setup.dart';
 import '../data/repo/ledger_current_month_repo_impl.dart';
 import '../domain/repo/ledger_current_month_repo.dart';
 import '../domain/use_case/ledger_current_month_use_case.dart';
+import '../presentation/bloc/ledger_current_month_bloc.dart';
 
 void ledgerCurrentMonthInit() {
   getIt
     ..registerSingleton<LedgerCurrentMonthRepo>(LedgerCurrentMonthRepoImpl(getIt(), getIt()))
-    ..registerSingleton<LedgerCurrentMonthUseCase>(LedgerCurrentMonthUseCase(getIt()));
+    ..registerSingleton<LedgerCurrentMonthUseCase>(LedgerCurrentMonthUseCase(getIt()))
+    ..registerFactory<LedgerCurrentMonthBloc>(() => LedgerCurrentMonthBloc(getIt()));
 }
