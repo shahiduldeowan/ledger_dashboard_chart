1dd934886f5a4fc240c9e8fa6713cb3f63a32b3f
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 17:28:22 +0600

    add BuildCurrentMonthInsights file

---
+++ lib/features/current_month/presentation/widgets/build_current_month_insights.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BuildCurrentMonthInsights extends StatelessWidget {
+  const BuildCurrentMonthInsights({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
