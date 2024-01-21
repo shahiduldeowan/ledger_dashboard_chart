746fd235363212a7c3d73dd5b0a3f649445f614a
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 15:22:51 +0600

    add BuildRemainChartIndicator widget

---
+++ lib/features/overview/presentation/widgets/build_remain_chart_indicator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/entity/ledger_overview_entity.dart';
+
+class BuildRemainChartIndicator extends StatelessWidget {
+  const BuildRemainChartIndicator({super.key, required this.overviewBalance});
+
+  final LedgerOverviewEntity overviewBalance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
