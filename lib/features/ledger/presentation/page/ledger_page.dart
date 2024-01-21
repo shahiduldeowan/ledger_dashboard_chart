7b605479b92af951ddc1184b6d382f87bc0b9cfd
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-21 14:08:14 +0600

    add page refresh indicator

--- lib/features/ledger/presentation/page/ledger_page.dart
+++ lib/features/ledger/presentation/page/ledger_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import '../../../../core/constants/app_sizes.dart';
 import '../../../../core/constants/strings.dart';
 import '../../../../core/utility/build_progress_indicator_widget.dart';
+import '../../../current_month/presentation/bloc/ledger_current_month_bloc.dart';
 import '../../../overview/domain/entity/ledger_overview_entity.dart';
 import '../../../overview/presentation/bloc/ledger_overview_bloc.dart';
 import '../../../overview/presentation/widgets/build_overview_chart_widget.dart';
@@ -28,7 +29,7 @@ class LedgerPage extends HookWidget {
         },
         builder: (context, overviewState) => switch (overviewState) {
           LedgerOverviewFailedState() => _buildEmptyWidget(),
-          LedgerOverviewLoadedState(overview: var overview) => _buildScaffoldBody(overview!),
+          LedgerOverviewLoadedState(overview: var overview) => _buildScaffoldBody(context, overview!),
           _ => buildProgressIndicatorWidget(),
         },
       ),
@@ -50,12 +51,21 @@ class LedgerPage extends HookWidget {
         ),
       );
 
-  ListView _buildScaffoldBody(LedgerOverviewEntity overview) {
-    return ListView(
-      padding: const EdgeInsets.all(AppSizes.padding),
-      children: [
-        BuildOverviewChart(overview: overview),
-      ],
+  RefreshIndicator _buildScaffoldBody(BuildContext context, LedgerOverviewEntity overview) {
+    return RefreshIndicator.adaptive(
+      onRefresh: () => _refreshHandler(context),
+      child: ListView(
+        padding: const EdgeInsets.all(AppSizes.padding),
+        children: [
+          BuildOverviewChart(overview: overview),
+        ],
+      ),
     );
+  }
+
+  Future<void> _refreshHandler(BuildContext context) {
+    context.read<LedgerOverviewBloc>().add(const GetLedgerOverviewEvent());
+    context.read<LedgerCurrentMonthBloc>().add(const GetLedgerCurrentMonthEvent());
+    return Future.delayed(const Duration(milliseconds: 3000));
   }
 }
